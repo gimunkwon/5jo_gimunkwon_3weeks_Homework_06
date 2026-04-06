6edd113b69967b54164f09f431f058c384977398
gimunkwon <gimon71@gmail.com>
2026-04-06 20:25:26 +0900

    [add] UnrealProject 기초 빌드 파일 업데이트

---
+++ Source/NBC_HomeWork_06/NBC_HomeWork_06.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NBC_HomeWork_06.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NBC_HomeWork_06, "NBC_HomeWork_06" );
